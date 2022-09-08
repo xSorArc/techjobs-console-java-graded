694bdf1d3950424e8c9857cdc07c7ada0995c24a
xSorArc <87716789+xSorArc@users.noreply.github.com>
2022-09-07 21:48:06 -0500

    Updated printJobs()

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -10,7 +10,7 @@ public class TechJobs {
 
     static Scanner in = new Scanner(System.in);
 
-    public static void main (String[] args) {
+    public static void main(String[] args) {
 
         // Initialize our field map with key/name pairs
         HashMap<String, String> columnChoices = new HashMap<>();
@@ -112,7 +112,7 @@ public class TechJobs {
                 validChoice = true;
             }
 
-        } while(!validChoice);
+        } while (!validChoice);
 
         return choiceKeys[choiceIdx];
     }
@@ -120,6 +120,17 @@ public class TechJobs {
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
 
-        System.out.println("printJobs is not implemented yet");
+        if (someJobs.size() == 0) {
+            System.out.print("No Results");
+        } else {
+            for (HashMap<String, String> job : someJobs) {
+                System.out.println("\n*****");
+
+                for (Map.Entry<String, String> j : job.entrySet()) {
+                    System.out.println(j.getKey() + ": " + j.getValue());
+                }
+                System.out.println("*****");
+            }
+        }
     }
 }
